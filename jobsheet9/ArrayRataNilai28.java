811cde2110ed81ba917c2e6d212a99eb7ed6d495
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-10-27 12:35:50 +0700

    modifikasi 1

--- jobsheet9/ArrayRataNilai28.java
+++ jobsheet9/ArrayRataNilai28.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
+
 public class ArrayRataNilai28 {
     public static void main(String[] args) {
-    
-    Scanner input = new Scanner(System.in);
-    int[] nilaiMhs = new int[10];
-    double total = 0;
-    double rata2;
 
-    for (int i = 0; i < nilaiMhs.length; i++) {
-        System.out.println("Masukkan niai mahasiswa ke-"+(i+1)+" : ");
-        nilaiMhs[i] = input.nextInt(); }
-        for (int i =0; i < nilaiMhs.length; i++) {
-            total +=nilaiMhs[i];
+        Scanner input = new Scanner(System.in);
+        int[] nilaiMhs = new int[10];
+        double total = 0;
+        double rata2;
+        int mahasiswaLulus = 0; 
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.println("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = input.nextInt();
+
+            if (nilaiMhs[i] > 70) {
+                mahasiswaLulus++;
+            }
         }
-            rata2 = total/nilaiMhs.length;
-            System.out.println("Rata-rata nilai ="+rata2);
-    
-}
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+        }
+
+        rata2 = total / nilaiMhs.length;
+        System.out.println("Rata-rata nilai = " + rata2);
+
+        System.out.println("Jumlah mahasiswa yang lulus = " + mahasiswaLulus);
+    }
 }
