64071fa17e05bc04b40e87a4472dd92af33cbdb0
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-10-29 20:30:28 +0700

    implementasi flowchart1 tugas mandiri pertemuan 9

---
+++ jobsheet9/ImplementasiFlowchart128.java
@@ -0,0 +1,10 @@
+public class ImplementasiFlowchart128 {
+    public static void main(String[] args) {
+        int[] arrayBilangan = new int[60];
+
+        for (int i = 0; i <60; i++) {  
+            arrayBilangan[i] = i+1;
+        System.out.println(arrayBilangan[i] );
+        }
+    }
+}
