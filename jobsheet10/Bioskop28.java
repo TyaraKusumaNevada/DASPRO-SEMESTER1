3e9a59b3e758717f02e44318b6a0bef5c510988d
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-01 09:53:04 +0700

    modifikasi 2.1 menggunakan for loop

--- jobsheet10/Bioskop28.java
+++ jobsheet10/Bioskop28.java
@@ -17,15 +17,22 @@ public class Bioskop28 {
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+
+        System.out.println("Nama penonton pada baris ke-3:");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
+        
+        
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
-        
+        System.out.println(penonton.length);
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);  
 
-        System.out.println("Panjang setiap baris penonton:");
-        for (String[] baris : penonton) {
-            System.out.println("Baris: " + baris.length);
         }
     }
-}
+
 
