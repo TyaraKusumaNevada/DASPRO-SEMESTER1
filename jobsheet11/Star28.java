34fcff44405371ce278bae3489418ed43a9544fd
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-08 13:08:41 +0700

    modifikasi pertanyaan no1 i=0

--- jobsheet11/Star28.java
+++ jobsheet11/Star28.java
@@ -7,7 +7,7 @@ public class Star28 {
         System.out.println("masukkan nilai N =") ;
         int N = input28.nextInt();
 
-        for(int i=1;i<=N; i++){
+        for(int i=0;i<=N; i++){
             System.out.print("*");
         }
 
