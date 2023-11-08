9e8563790c45ec425c04aaa1340f515fcc56e1b6
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-08 12:48:08 +0700

    percobaan 1

---
+++ jobsheet11/Star28.java
@@ -0,0 +1,16 @@
+package jobsheet11;
+
+import java.util.Scanner;
+public class Star28 {
+    public static void main(String[] args) {
+        Scanner input28 = new Scanner(System.in);
+        System.out.println("masukkan nilai N =") ;
+        int N = input28.nextInt();
+
+        for(int i=1;i<=N; i++){
+            System.out.print("*");
+        }
+
+    }
+    
+}
