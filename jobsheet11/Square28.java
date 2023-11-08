e3cc154f0049656b4844d86fa19c667d060df315
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-08 13:58:32 +0700

    Percobaan 2

---
+++ jobsheet11/Square28.java
@@ -0,0 +1,20 @@
+package jobsheet11;
+import java.util.Scanner;
+
+public class Square28 {
+    public static void main(String[] args) {
+        Scanner input28 = new Scanner(System.in);
+        System.out.print("Masukkan Nilai N = ");
+        int N = input28.nextInt();
+
+        for (int iOuter = 1; iOuter <= N; iOuter++) {
+
+            for (int i = 1; i <= N; i++) {
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+    }
+}
+
+
