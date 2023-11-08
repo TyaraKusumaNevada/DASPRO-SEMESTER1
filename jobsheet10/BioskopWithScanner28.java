e2d5b15c8f8e3bba17703fee7382ce4bc685d6e8
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-08 08:13:00 +0700

    Modifikasi kode program untuk opsi menu

--- jobsheet10/BioskopWithScanner28.java
+++ jobsheet10/BioskopWithScanner28.java
@@ -1,26 +1,58 @@
 package jobsheet10;
 import java.util.Scanner;
+
 public class BioskopWithScanner28 {
     public static void main(String[] args) {
-
         Scanner input28 = new Scanner(System.in);
         int baris, kolom;
         String nama, next;
         String[][] penonton = new String[4][2];
 
-
         while (true) {
-            System.out.println("masukkan nama: ");
-            nama = input28.nextLine();
-            System.out.println("masukkan baris");
-            baris = input28.nextInt();
-            System.out.println("masukkan kolom");
-            kolom = input28.nextInt();
-            input28.nextLine();
-            
-            penonton[baris -1][kolom-1]= nama;
+            System.out.println("Menu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu (1/2/3): ");
+            int choice = input28.nextInt();
+            input28.nextLine(); 
 
-            System.out.println("input penonton lainnya? (y/n): ");
+            switch (choice) {
+                case 1:
+                    System.out.println("Masukkan nama: ");
+                    nama = input28.nextLine();
+                    System.out.println("Masukkan baris: ");
+                    baris = input28.nextInt();
+                    System.out.println("Masukkan kolom: ");
+                    kolom = input28.nextInt();
+                    input28.nextLine();
+
+                    // Periksa validitas baris dan kolom
+                    if (baris >= 1 && baris <= 4 && kolom >= 1 && kolom <= 2) {
+                        penonton[baris - 1][kolom - 1] = nama;
+                        System.out.println("Data penonton berhasil dimasukkan.");
+                    } else {
+                        System.out.println("Baris atau kolom tidak valid. Data penonton tidak dimasukkan.");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Daftar Penonton:");
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            if (penonton[i][j] != null) {
+                                System.out.println("Baris " + (i + 1) + ", Kolom " + (j + 1) + ": " + penonton[i][j]);
+                            }
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Terima kasih!");
+                    System.exit(0);
+                default:
+                    System.out.println("Pilihan menu tidak valid. Silakan pilih 1, 2, atau 3.");
+            }
+
+            System.out.print("Lanjutkan? (y/n): ");
             next = input28.nextLine();
 
             if (next.equalsIgnoreCase("n")) {
@@ -28,5 +60,5 @@ public class BioskopWithScanner28 {
             }
         }
     }
-    
+            
 }
