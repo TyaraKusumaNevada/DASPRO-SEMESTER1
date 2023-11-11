86f8e91a20f1e655f21c595141373e633615a061
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-11 20:14:49 +0700

    Tugas no3

--- jobsheet11/TugasPersegiAngka28.java
+++ jobsheet11/TugasPersegiAngka28.java
@@ -3,27 +3,25 @@ import java.util.Scanner;
 public class TugasPersegiAngka28 {
    
 
-public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan N: ");
-        int n = sc.nextInt();
-        int arr[][] = new int[n][n];
+    public static void main(String[] args) {
+        Scanner sc28 = new Scanner(System.in);
+        System.out.print("Masukkan N = ");
+        int n = sc28.nextInt();
+        int[][] arr = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                arr[i][j] = n;
+                if (i == 0 || i == n-1 || j == 0 || j == n-1) {
+                    arr[i][j] = 5;
+                } else if (i == j || i+j == n-1) {
+                    arr[i][j] = 3;
+                }
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || i == (n - 1)) {
-                    System.out.print(arr[i][j]);
-                } else if (j == 0 || j == (n - 1)) {
-                    System.out.print(arr[i][j]);
-                } else {
-                    System.out.print(" ");
-                }
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
