57336d69f946a9140f66148611cc76370b709599
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-22 12:41:02 +0700

    1.2 Modifikasi tanpa fungsi

--- jobsheet13/Terimakasih_28.java
+++ jobsheet13/Terimakasih_28.java
@@ -1,11 +1,8 @@
 package jobsheet13;
 
 public class Terimakasih_28 {
-    public static void UcapanTerimakasih() {
-        System.out.println("Thankyou for being the best teacher in the world. \n"+
-        "You inspired in me a love for learning and made me feel like i could ask you anything.");
-    }
     public static void main(String[] args) {
-        UcapanTerimakasih();
+        System.out.println("Thank you for being the best teacher in the world. \n" +
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
 }
