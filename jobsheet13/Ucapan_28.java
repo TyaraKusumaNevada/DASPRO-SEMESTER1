618cc401a6fe25ee8fe54c9b177023adaa9a2d28
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-22 14:05:10 +0700

    Percobaan 3

---
+++ jobsheet13/Ucapan_28.java
@@ -0,0 +1,19 @@
+package jobsheet13;
+
+import java.util.Scanner;
+
+public class Ucapan_28 {
+    public static String PenerimaUcapan(){  
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuiskan Nama Orang yang ingin anda beri ucapan: ");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+       }
+    
+       public static void main(String[] args) {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " +nama+"\nMay the force be with you. ");
+       }
+
+}
