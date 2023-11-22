9350909a86cde712f20e2adba2dd04dde958b533
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-22 13:28:55 +0700

    Percobaan 2

--- jobsheet13/Terimakasih_28.java
+++ jobsheet13/Terimakasih_28.java
@@ -1,8 +1,19 @@
 package jobsheet13;
 
 public class Terimakasih_28 {
+    public static void UcapanTerimakasih() {
+        System.out.println("Thankyou for being the best teacher in the world. \n"+
+        "You inspired in me a love for learning and made me feel like i could  ask you anything.");   
+    }
+    public static void UcapanTambahan (String Ucapan) {
+        System.out.println(Ucapan);
+    }
+    
+    
     public static void main(String[] args) {
-        System.out.println("Thank you for being the best teacher in the world. \n" +
-        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+        UcapanTerimakasih();
+        String ucapan = "Terimakasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
+
