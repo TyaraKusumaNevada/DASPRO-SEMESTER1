0286a52b7400dbcadc0ced170054c09acd8718bf
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-22 07:05:31 +0700

    Percobaan 1

---
+++ jobsheet13/Terimakasih_28.java
@@ -0,0 +1,11 @@
+package jobsheet13;
+
+public class Terimakasih_28 {
+    public static void UcapanTerimakasih() {
+        System.out.println("Thankyou for being the best teacher in the world. \n"+
+        "You inspired in me a love for learning and made me feel like i could ask you anything.");
+    }
+    public static void main(String[] args) {
+        UcapanTerimakasih();
+    }
+}
