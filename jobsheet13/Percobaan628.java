00157d3269ae8e636095cf677bdbea420d78e877
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-22 16:06:17 +0700

    Percobaan 6

---
+++ jobsheet13/Percobaan628.java
@@ -0,0 +1,48 @@
+package jobsheet13;
+
+import java.util.Scanner;
+
+public class Percobaan628 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       
+        int p,l,t,L,vol;
+
+    System.out.println("Masukkan panjang");
+    p=input.nextInt();
+    System.out.println("Masukkan lebar");
+    l=input.nextInt();
+    System.out.println("Masukkan tinggi");
+    t= input.nextInt();
+    
+    L=p*l;
+    System.out.println("Luas persegi panjang adalah "+L);
+
+    vol=p*l*t;
+    System.out.println("Volume balok adalah "+vol);
+    }
+    static int hitungLuas (int pjg, int lb){
+        int Luas= pjg*lb;
+        return Luas;
+    }
+    static int hitungVolume (int tinggi, int a, int b){
+        int volume = hitungLuas(a,b)*tinggi;
+        return volume;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int p,l,t,L,vol;
+        System.out.println("Masukkan panjang");
+        p= input.nextInt();
+        System.out.println("Masukkan lebar");
+        l=input.nextInt();
+        System.out.println("Masukkan tinggi");
+        t=input.nextInt();
+
+        L=hitungLuas(p,L);
+        System.out.println("Luas persegi panjang adalah "+L);
+        vol= hitungVolume(t, L, vol);
+        System.out.println(" Volume Balok adalah "+vol);
+    }
+}
