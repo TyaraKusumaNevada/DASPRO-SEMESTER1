835c8cd26bc29fed451548dacda93e13eacb3d5d
TyaraKusumaNevada <tyaranevada29@gmail.com>
2023-11-28 23:43:58 +0700

    modifikasi pertanyaan no.3

--- jobsheet13/nilai28.java
+++ jobsheet13/nilai28.java
@@ -3,23 +3,32 @@ package jobsheet13;
 import java.util.Scanner;
 
 public class nilai28 {
-    static int jumlahMahasiswa = 5;
-    static int jumlahMinggu = 7;
+
+    static int jumlahMahasiswa;
+    static int jumlahMinggu;
     static int nilaiTertinggi = 0;
     static int mingguNilaiTertinggi = 0;
 
     private static int[][] nilaiMahasiswa = new int[jumlahMahasiswa][jumlahMinggu];
 
     public static void main(String[] args) {
+        inputJumlahTugasMhs();
         jumlahNilaiMahasiswa();
         tampilNilaiMahasiswa();
         NilaiTertinggi();
         tampilMahasiswaNilaiTertinggi();
     }
+    private static void inputJumlahTugasMhs(){
+        Scanner input= new Scanner(System.in);
+        System.out.println("Masukkan jumlah mahasiswa: ");
+        jumlahMahasiswa=input.nextInt();
+        System.out.println("Masukkan jumlah tugas per minggu: ");
+        jumlahMinggu=input.nextInt();
+        nilaiMahasiswa= new int [jumlahMahasiswa][jumlahMinggu];
+    }
 
     private static void jumlahNilaiMahasiswa() {
         Scanner input = new Scanner(System.in);
-
         for (int i = 0; i < jumlahMahasiswa; i++) {
             System.out.println("Masukkan data nilai mahasiswa ke-" + (i + 1));
             for (int j = 0; j < jumlahMinggu; j++) {
@@ -28,7 +37,6 @@ public class nilai28 {
             }
         }
     }
-
     private static void tampilNilaiMahasiswa() {
         System.out.println("Nilai Tugas Mahasiswa:");
         for (int i = 0; i < jumlahMahasiswa; i++) {
@@ -39,9 +47,8 @@ public class nilai28 {
             System.out.println();
         }
     }
-
     private static void NilaiTertinggi() {
-
+        
         for (int i = 0; i < jumlahMahasiswa; i++) {
             for (int j = 0; j < jumlahMinggu; j++) {
                 if (nilaiMahasiswa[i][j] > nilaiTertinggi) {
@@ -52,7 +59,6 @@ public class nilai28 {
         }
         System.out.println("Nilai tertinggi adalah " + nilaiTertinggi + " pada minggu ke-" + mingguNilaiTertinggi);
     }
-
     private static void tampilMahasiswaNilaiTertinggi() {
         int nilaiTertinggi = 0;
         int mahasiswaDenganNilaiTertinggi = 0;
@@ -64,9 +70,6 @@ public class nilai28 {
                 }
             }
         }
-
         System.out.println("Mahasiswa dengan nilai tertinggi adalah mahasiswa ke-" + (mahasiswaDenganNilaiTertinggi + 1) + " dengan nilai " + nilaiTertinggi + " pada minggu ke-" + mingguNilaiTertinggi);
     }
 }
-    
-
